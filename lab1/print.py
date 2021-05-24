457e8cf3e5bfb8c17f72b99b8703c0398944f432
Aqil <aqilzeka99@gmail.com>
2021-05-24 06:44:02 +0400

    Complete lab 1

---
+++ lab1/print.py
@@ -0,0 +1,28 @@
+import sys
+
+# Practice writing comment
+print("Hello World!")
+
+print(sys.version)  # This line prints version of python
+
+print(type(12))
+print(type(12.))
+print(type(0))
+print(type(True))
+print(type("True"))
+
+print(sys.float_info)
+print(sys.int_info)
+
+# Converting from one object type to a different object type
+print(float(2))
+
+print(int('1'))
+print(int(1.111))
+
+print(type(6 / 2))  # Float
+
+print(type(6 // 2))  # int
+
+print(160 // 60)
+print(160 % 60)
